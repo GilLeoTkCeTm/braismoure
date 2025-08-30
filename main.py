a744348dea7a51e17422afde9437387427f65331
gilbert.bothia <gc.azure@ieb.co>
2025-08-29 22:28:09 -0500

    guardado de proyecto con nota de video y minuto donde se va

--- main.py
+++ main.py
@@ -1,1 +1,2 @@
-print("Hello main since main, este esun complemento para el stash)
+print("Hello main since main, este esun complemento para el stash")
+"""https://www.youtube.com/watch?v=3GymExBkKjE&t=2876s    min2.47.36 authentication ssh"""
